7b22f413b6b28a017860db7c428cbc305f8af7c8
zsg0428 <zsg258852@gmail.com>
2024-06-18 15:41:28 -0400

    adding open and close modal, and new css

--- lululemon/src/components/productpage/ProductPage.js
+++ lululemon/src/components/productpage/ProductPage.js
@@ -5,6 +5,7 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {myKey, productURL, singleProductURL} from "../../redux/helper";
 import './ProductPage.scss'
+import {Modal} from "./Modal";
 
 export const ProductPage = () => {
     // Router
@@ -20,6 +21,7 @@ export const ProductPage = () => {
     const [swatchName, setSwatchName] = useState('')
     const [selectedSize, setSelectedSize] = useState('')
     const [isSizeSelected, setIsSizeSelected] = useState(false)
+    const [isModalVisible, setIsModalVisible] = useState(false)
 
     useEffect(() => {
         const params = new URLSearchParams(location.search)
@@ -50,27 +52,24 @@ export const ProductPage = () => {
     }, [productID]);
 
     // Define a function to get the images based on selected swatch
-    const getCurrentImages = () => {
+    const getCurrentImagesAndAlts = () => {
         const currentImages = product.images?.find(image => image.colorId === selectedColorId)
         if (currentImages) {
-            const imagesUrl = currentImages.mainCarousel.media.split('|').map(img => img.trim())
-            return imagesUrl
+            return {
+                images: currentImages.mainCarousel.media.split('|').map(img => img.trim()),
+                alt: currentImages.mainCarousel.alt
+            }
         }
-        return []
+        return {
+            images: [],
+            alt: 'No Alt'
+        }
     }
     // Define a function to get
     // the images's alt based on selected swatch
-    const getCurrentImageAlt = () => {
-        const currentImages = product.images?.find(image => image.colorId === selectedColorId)
-        if (currentImages) {
-            const currentAlt = currentImages.mainCarousel.alt
-            return currentAlt
-        }
-        return 'No Alt'
-    }
 
-    const images = getCurrentImages()
-    const alt = getCurrentImageAlt()
+    const {images, alt} = getCurrentImagesAndAlts()
+
 
     const handleSwatchClick = (colorID, index, swatchName) => {
         setSelectedColorId(colorID)
@@ -85,6 +84,14 @@ export const ProductPage = () => {
         setIsSizeSelected(true)
     }
 
+    const handleModalOpen = () => {
+        setIsModalVisible(true)
+    }
+
+    const handleModalClose = () => {
+        setIsModalVisible(false)
+    }
+
     if (!product) {
         return <div>loading</div>
     }
@@ -92,30 +99,32 @@ export const ProductPage = () => {
 
     return (
         <>
-            <Header/>
-            <div className='productPageContainer'>
-                <h3>this is productID : {productID}</h3>
-                <div className='productInfoContainer'>
+            <div className={isModalVisible === true ? 'productPageWrapperHidden' : 'productPageWrapperActive'}>
+                <Header/>
+                <div className='productPageContainer'>
+                    <div className='productInfoContainer'>
 
-                    <div className='productImagesContainer'>
-                        {images.map((image, index) => {
-                                return <img className='images' key={index} src={image}
-                                            alt={alt}/>
-                            }
-                        )}
-                    </div>
-                    <div className='productInfo'>
-                        <div className='productName'>{product.name}</div>
-                        <div className='productPrice'>{product.price}</div>
-
-
-                        <div className='colorWord'>Colour
-                            <div className='colorName wordStyle'>{swatchName}</div>
+                        <div className='productImagesContainer'>
+                            {images.map((image, index) => {
+                                    return <img className='images' key={index} src={image}
+                                                alt={alt}
+                                                onClick={handleModalOpen}
+                                    />
+                                }
+                            )}
                         </div>
-                        <div className='swatchesContainer'>
-                            {product.swatches && product.swatches.map((swatch, index) => {
-                                return (
-                                    <>
+                        <div className='productInfo'>
+                            <div className='productName'>{product.name}</div>
+                            <div className='productPrice'>{product.price}</div>
+
+
+                            <div className='colorWord'>Colour
+                                <div className='colorName wordStyle'>{swatchName}</div>
+                            </div>
+                            <div className='swatchesContainer'>
+                                {product.swatches && product.swatches.map((swatch, index) => {
+                                    return (
+
                                         <button key={index}
                                                 className='swatchButton'
                                                 onClick={() => handleSwatchClick(swatch.colorId, index, swatch.swatchAlt)}>
@@ -124,45 +133,48 @@ export const ProductPage = () => {
                                                  alt={swatch.swatchAlt}
                                             />
                                         </button>
-                                    </>
-                                )
-                            })}
-                        </div>
-                        <div className='sizeContainer'>
-                            {product.sizes && product.sizes.map((sizeGroup, index) => {
-                                return (
-                                    <div key={index}>
-                                        <div className='selectSizeWord'>{isSizeSelected ? 'Size' : sizeGroup.title}
-                                            <div className='wordStyle'> {selectedSize}</div>
-                                        </div>
-                                        <div className='sizeButtonsContainer'>
-                                            {sizeGroup.details.map((size, i) =>
-                                                <button
-                                                    className={`${selectedSizeIndex === i ? 'sizeLettersButtonChecked' : 'sizeLettersButton'} `}
-                                                    key={i}
-                                                    onClick={() => handleSizeButtonClick(size, i)}
-                                                >{size ? size : 'nosize'}</button>
-                                            )}
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
 
+                                    )
+                                })}
+                            </div>
+                            <div className='sizeContainer'>
+                                {product.sizes && product.sizes.map((sizeGroup, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <div className='selectSizeWord'>{isSizeSelected ? 'Size' : sizeGroup.title}
+                                                <div className='wordStyle'> {selectedSize}</div>
+                                            </div>
+                                            <div className='sizeButtonsContainer'>
+                                                {sizeGroup.details.map((size, i) =>
+                                                    <button
+                                                        className={`${selectedSizeIndex === i ? 'sizeLettersButtonChecked' : 'sizeLettersButton'} `}
+                                                        key={i}
+                                                        onClick={() => handleSizeButtonClick(size, i)}
+                                                    >{size ? size : 'nosize'}</button>
+                                                )}
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                                <div className='soldoutWord'>Size sold out? Select size to get notified</div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
+                {/*Details go here*/}
+                <div>Why We Made This
+                    <div>{product.whyWeMadeThis}</div>
+                </div>
+                <div>
+                    {/*底下这俩都是返回Whats New Page。看你们爱用哪个都行*/}
+                    <Link to='/'>To What's New Page </Link>
+                    <br/>
+                    <button onClick={() => navigate('/')}>Go Back to What's New Page</button>
+                </div>
+                <Footer/>
             </div>
-            {/*Details go here*/}
-            <div>Why We Made This
-                <div>{product.whyWeMadeThis}</div>
-            </div>
-            <div>
-                {/*底下这俩都是返回Whats New Page。看你们爱用哪个都行*/}
-                <Link to='/'>To What's New Page </Link>
-                <br/>
-                <button onClick={() => navigate('/')}>Go Back to What's New Page</button>
-            </div>
-            <Footer/>
+            {isModalVisible && <Modal images={images} close={handleModalClose} alt={alt} name={product.name}/>}
         </>
     )
 }
