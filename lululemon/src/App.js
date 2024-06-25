bcb6a74a875c07264f0a559bca2ace654cac7f4b
Bruce Wang <bruce.wang15@outlook.com>
2024-06-24 23:27:17 -0700

    order summary layout complete

--- lululemon/src/App.js
+++ lululemon/src/App.js
@@ -4,10 +4,12 @@ import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import {ProductPage} from "./components/productpage/ProductPage";
 import {WrongPage} from './components/productpage/WrongPage'
 import {WrongProductPage} from "./components/productpage/WrongProductPage";
+import OrderSummary from "./components/mybag/OrderSummary";
 
 function App() {
     return (
         <div className="App">
+            <OrderSummary/>
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<WhatsNewPage/>}/>
