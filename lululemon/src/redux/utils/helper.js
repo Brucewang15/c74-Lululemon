c90522f199173de7f7c86fbde7be9797b1ce6c10
jayzchen2001 <jayzchen2001@gmail.com>
2024-08-07 17:07:03 -0400

    added inventory

--- lululemon/src/redux/utils/helper.js
+++ lululemon/src/redux/utils/helper.js
@@ -16,6 +16,8 @@ export const myKey =
 export const productURL = `http://api-lulu.hibitbyte.com/product/allProducts?page=1&mykey=${myKey}`;
 export const filterURL = `http://api-lulu.hibitbyte.com/product/filter?mykey=${myKey}`;
 
+export const inventoryURL = `//localhost:3399/inv/item/id:1`;
+
 // export const productURL = 'http://api-lulu.hibitbyte.com/product/allProducts?page=1&mykey=QNshco%2Bkkoxz2b5FtiitfQZdaTwKo%2Bhnc6f50d1LWDP6JtjB7fAjd5tFFjeBhCS2l/3Yq4batruGmWrcvq2T6w==';
 // export const filterURL = 'http://api-lulu.hibitbyte.com/product/filter?mykey=QNshco%2Bkkoxz2b5FtiitfQZdaTwKo%2Bhnc6f50d1LWDP6JtjB7fAjd5tFFjeBhCS2l/3Yq4batruGmWrcvq2T6w==';
 
